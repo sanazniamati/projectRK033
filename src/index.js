861083a8845828140bea0ef7fb779f878916769f
sanazniamati <sanaz.niamati1991@gmail.com>
2022-06-22 13:06:48 +0430

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Stage, Layer, Group, Rect } from "react-konva";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  function generateShapes() {
+    const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+    return [...Array(6)].map((_, i) => ({
+      id: i.toString(),
+      x: i * 30 + 10,
+      y: i * 30 + 10,
+      colors: colors[i],
+    }));
+  }
+  const INITIAL_STATE = generateShapes();
+  const [rects, setRects] = useState(INITIAL_STATE);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        {rects.map((star) => (
+          <Group draggable>
+            <Rect
+              key={star.id}
+              x={star.x}
+              y={star.y}
+              width={100}
+              height={50}
+              fill={star.colors}
+              stroke={"black"}
+              strokeWidth={4}
+            />
+          </Group>
+        ))}
+      </Layer>
+    </Stage>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
